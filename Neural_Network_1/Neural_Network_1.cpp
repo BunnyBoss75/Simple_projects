9feb9fc5b835e93f7f2e55dbf7b352176c94bd49
BunnyBoss75 <BunnyBoss75@users.noreply.github.com>
2020-02-05 21:14:08 +0200

    first commit

---
+++ Neural_Network_1/Neural_Network_1.cpp
@@ -0,0 +1,174 @@
+﻿// A - array with data, other variables in one letter - counters
+#include <fstream>
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+double F_result(double** Coefficients, double** Data, int amount);
+double Sigma(double x) {
+	return 1 / (1 + exp(-x));
+}
+int main() {
+	int i = 0, j = 0;
+	int best_coefficients = 0, best_i = 0, best_j = 0;
+	double best_result = 0;
+	double coef = 0.001;
+	int amount = 2, check1 = 0;
+	int count_1 = 0;
+	double previous, now;
+	char* context;
+	ifstream train("train.txt");
+	char** Train = new char*[amount];
+	for (i = 0; i < amount; i++) {
+		Train[i] = new char[1000];
+		train >> Train[i];
+	}
+	string** Train1 = new string*[amount];
+	for (i = 0; i < amount; i++)
+		Train1[i] = new string[13];
+	for (i = 0; i < amount; i++) {
+		Train1[i][0] = strtok_s(Train[i], ",", &context);
+		for (j = 1; j < 13; j++)
+			Train1[i][j] = strtok_s(NULL, ",", &context);
+	}
+	double** A = new double* [amount];
+	for (i = 0; i < amount; i++)
+		A[i] = new double[11];
+	for (i = 0; i < amount; i++) {
+		A[i][0] = atof(Train1[i][1].c_str()); // Survived
+		if (Train1[i][2] == "$")
+			A[i][1] = 0;
+		else
+			A[i][1] = atof(Train1[i][2].c_str()); // Pclass
+		Train1[i][3] += Train1[i][4];
+		const char* temporality = Train1[i][3].c_str();
+		A[i][2] = 0;
+		for (j = 0; j < strlen(temporality); j++)
+			A[i][2] += (int)temporality[j];
+		A[i][2] *= 0.01; // Name
+		if (Train1[i][5] == "male")
+			A[i][3] = 1;
+		else
+			A[i][3] = 2; // Sex
+		if (Train1[i][6] == "$")
+			A[i][4] = 0;
+		else
+			A[i][4] = atof(Train1[i][6].c_str()); // Age
+		if (Train1[i][7] == "$")
+			A[i][5] = 0;
+		else
+			A[i][5] = atof(Train1[i][7].c_str()); // SibSp
+		if (Train1[i][8] == "$")
+			A[i][6] = 0;
+		else
+			A[i][6] = atof(Train1[i][8].c_str()); // Parch
+		A[i][7] = 0;
+		if (Train1[i][9] != "$") {
+			temporality = Train1[i][9].c_str();
+			for (j = 0; j < strlen(temporality); j++)
+				A[i][7] += (int)temporality[j];
+			A[i][7] *= 0.01; // Ticket
+		}
+		if (Train1[i][10] == "$")
+			A[i][8] = 0;
+		else
+			A[i][8] = atof(Train1[i][10].c_str()) * 0.1; // Fare
+		A[i][9] = 0;
+		if (Train1[i][11] != "$") {
+			temporality = Train1[i][11].c_str();
+			for (j = 0; j < strlen(temporality); j++)
+				A[i][9] += (int)temporality[j];
+			A[i][9] *= 0.01; // Cabin
+		}
+		if (Train1[i][12] == "$")
+			A[i][10] = 0;
+		else if (Train1[i][12] == "S")
+			A[i][10] = 1;
+		else if (Train1[i][12] == "C")
+			A[i][10] = 2;
+		else
+			A[i][10] = 3; // Embarked
+	}
+	train.close();
+	ifstream IFcoefficients("coefficient.txt");
+	double** Coefficients = new double* [51];
+	for (i = 0; i < 51; i++) {
+		Coefficients[i] = new double[10];
+		for (j = 0; j < 10; j++)
+			IFcoefficients >> Coefficients[i][j];
+	}
+	IFcoefficients.close();
+	previous = F_result(Coefficients, A, amount);
+	best_result = previous;
+	while (1) {
+		for (i = 0; i < 510; i++) {
+			for (j = -1; j < 2; j += 2) {
+				Coefficients[(i - (i % 10)) / 10][i % 10] += coef * (double)j * (100.0 - previous);
+				if (Coefficients[(i - (i % 10)) / 10][i % 10] < 100 && Coefficients[(i - (i % 10)) / 10][i % 10] > -100) {
+					now = F_result(Coefficients, A, amount);
+					if (now > best_result) {
+						best_i = i;
+						best_j = j;
+						best_result = now;
+					}
+				}
+				Coefficients[(i - (i % 10)) / 10][i % 10] -= coef * (double)j * (100.0 - previous);
+			}
+		}
+		if (best_result > previous) {
+			Coefficients[(best_i - (best_i % 10)) / 10][best_i % 10] += coef * (double)best_j * (100.0 - previous);
+			count_1++;
+			if (count_1 % 1 == 0)
+				cout << "result is: " << best_result << " up is: " << best_result - previous << " delta is " << coef * (double)best_j * (100.0 - previous) <<" coef is: " << best_i << " count_ is: " << count_1 << "\n";
+			if (count_1 == 1000)
+				break;
+			previous = best_result;
+			continue;
+		}
+		else {
+			cout << "It is the end\n";
+			break;
+		}
+	}
+	ofstream OFcoefficients("coefficient.txt");
+	for (i = 0; i < 51; i++) {
+		for (j = 0; j < 10; j++)
+			OFcoefficients << Coefficients[i][j] << " ";
+		OFcoefficients << "\n";
+	}
+	OFcoefficients.close();
+	system("pause");
+}
+double F_result(double** Coefficients, double** Data, int amount) {
+	int i = 0, j = 0, x = 0, k = 0;
+	double result = 0, result1 = 0;
+	double** A = new double* [6];
+	for (i = 0; i < 6; i++)
+		A[i] = new double[10];
+	for (x = 0; x < amount; x++) {
+		bool survived = Data[x][0];
+		for (i = 0; i < 11; i++)
+			A[0][i] = Data[x][i + 1];
+		for (i = 1; i < 6; i++)
+			for (j = 0; j < 10; j++) {
+				A[i][j] = 0;
+				for (k = 0; k < 10; k++)
+					A[i][j] += A[i - 1][k] * Coefficients[(i - 1) * 10 + j][k];
+				A[i][j] *= 0.1;
+			}
+		result1 = 0;
+		for (i = 0; i < 10; i++)
+			result1 += A[5][i] * Coefficients[50][i];
+		result1 = 100 * Sigma(result1);
+	    //cout << result1 << "    " << survived << "\n";
+		if (survived)
+			result += result1;
+		else
+			result += (100 - result1);
+	}
+	for (i = 0; i < 6; i++) {
+		delete A[i];
+	}
+	delete[] A;
+	return result/amount;
+}
---
+++ Neural_Network_2/Neural_Network_2.cpp
@@ -0,0 +1,314 @@
+﻿#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <string>
+using namespace std;
+class NeuralNetwork {
+	private:
+		int amount_of_level, amount_of_neurons_per_level, amount;
+		double Sigma(double x) {
+			return 1 / (1 + exp(-x));
+		}
+		double F_result(double** Coefficients, double* Data) {
+			bool survived = Data[0];
+			double** Neurons = new double* [amount_of_level];
+			for (int i = 0; i < amount_of_level; i++) {
+				Neurons[i] = new double[amount_of_neurons_per_level];
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					Neurons[i][j] = 0;
+				}
+			}
+			for (int j = 0; j < amount_of_neurons_per_level; j++) {
+				Neurons[0][j] = Data[j + 1];
+				Neurons[0][j] = Sigma(Neurons[0][j]);
+			}
+			for (int i = 1; i < amount_of_level; i++) {
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					for (int k = 0; k < amount_of_neurons_per_level; k++) {
+						Neurons[i][j] += Neurons[i - 1][k] * Coefficients[(i - 1) * amount_of_neurons_per_level + j][k];
+					}
+					Neurons[i][j] = Sigma(Neurons[i][j]);
+				}
+			}
+			double result = 0;
+			for (int i = 0; i < amount_of_neurons_per_level; i++) {
+				result += Neurons[amount_of_level - 1][i] * Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][i];
+			}
+			result = Sigma(result);
+			for (int i = 0; i < amount_of_level; i++) {
+				delete Neurons[i];
+			}
+			delete[] Neurons;
+			if (survived) {
+				return result * 100;
+			}
+			else {
+				return 100 - (result * 100);
+			}
+		}
+		double** Read_coefficients() {
+			double** Coefficients = new double* [(amount_of_level - 1) * amount_of_neurons_per_level + 1];
+			for (int i = 0; i < (amount_of_level - 1) * amount_of_neurons_per_level + 1; i++) {
+				Coefficients[i] = new double[amount_of_neurons_per_level];
+			}
+			ifstream IFcoefficients("coefficient.txt");
+			for (int i = 0; i < (amount_of_level - 1) * amount_of_neurons_per_level + 1; i++) {
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					IFcoefficients >> Coefficients[i][j];
+				}
+			}
+			IFcoefficients.close();
+			return Coefficients;
+		}
+		double** Read_Train() {
+			ifstream IFtrain("train.txt");
+			char** Train = new char* [amount];
+			for (int i = 0; i < amount; i++) {
+				Train[i] = new char[1000];
+				IFtrain >> Train[i];
+			}
+			string** Train1 = new string * [amount];
+			for (int i = 0; i < amount; i++) {
+				Train1[i] = new string[13];
+			}
+			for (int i = 0; i < amount; i++) {
+				Train1[i][0] = strtok(Train[i], ",");
+				for (int j = 1; j < 13; j++)
+					Train1[i][j] = strtok(NULL, ",");
+			}
+			double** Data = new double* [amount];
+			for (int i = 0; i < amount; i++)
+				Data[i] = new double[11];
+			for (int i = 0; i < amount; i++) {
+				Data[i][0] = atof(Train1[i][1].c_str()); // Survived
+				if (Train1[i][2] == "$")
+					Data[i][1] = 0;
+				else
+					Data[i][1] = atof(Train1[i][2].c_str()); // Pclass
+				Train1[i][3] += Train1[i][4];
+				const char* temporality = Train1[i][3].c_str();
+				Data[i][2] = 0;
+				for (int j = 0; j < strlen(temporality); j++)
+					Data[i][2] += (int)temporality[j];
+				Data[i][2] *= 0.01; // Name
+				if (Train1[i][5] == "male")
+					Data[i][3] = 1;
+				else
+					Data[i][3] = 2; // Sex
+				if (Train1[i][6] == "$")
+					Data[i][4] = 0;
+				else
+					Data[i][4] = atof(Train1[i][6].c_str()); // Age
+				if (Train1[i][7] == "$")
+					Data[i][5] = 0;
+				else
+					Data[i][5] = atof(Train1[i][7].c_str()); // SibSp
+				if (Train1[i][8] == "$")
+					Data[i][6] = 0;
+				else
+					Data[i][6] = atof(Train1[i][8].c_str()); // Parch
+				Data[i][7] = 0;
+				if (Train1[i][9] != "$") {
+					temporality = Train1[i][9].c_str();
+					for (int j = 0; j < strlen(temporality); j++)
+						Data[i][7] += (int)temporality[j];
+					Data[i][7] *= 0.01; // Ticket
+				}
+				if (Train1[i][10] == "$")
+					Data[i][8] = 0;
+				else
+					Data[i][8] = atof(Train1[i][10].c_str()) * 0.1; // Fare
+				Data[i][9] = 0;
+				if (Train1[i][11] != "$") {
+					temporality = Train1[i][11].c_str();
+					for (int j = 0; j < strlen(temporality); j++)
+						Data[i][9] += (int)temporality[j];
+					Data[i][9] *= 0.01; // Cabin
+				}
+				if (Train1[i][12] == "$")
+					Data[i][10] = 0;
+				else if (Train1[i][12] == "S")
+					Data[i][10] = 1;
+				else if (Train1[i][12] == "C")
+					Data[i][10] = 2;
+				else
+					Data[i][10] = 3; // Embarked
+			}
+			IFtrain.close();
+			return Data;
+		}
+	public:
+		void Create_Coefficients() {
+			srand(time(NULL));
+			ofstream OFcoefficients("coefficient.txt");
+			for (int i = 0; i < (amount_of_level - 1) * amount_of_neurons_per_level + 1; i++) {
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					OFcoefficients << ((rand() % 20) - 10) / 10.0 << " ";
+				}
+				OFcoefficients << endl;
+			}
+			OFcoefficients.close();
+		}
+		NeuralNetwork (int value_amount_of_level, int value_amount_of_neurons_per_level, int value_amount){
+			amount_of_level = value_amount_of_level;
+			amount_of_neurons_per_level = value_amount_of_neurons_per_level;
+			amount = value_amount;
+		}
+		void Learn_1(int amount_of_iteration, double coefficient_of_delta) {
+			double** Coefficients = Read_coefficients();
+			double** Data = Read_Train();
+			double delta = 0;
+			int amount_of_coefficients = ((amount_of_level - 1) * amount_of_neurons_per_level + 1) * amount_of_neurons_per_level;
+			for (int i = 0; i < amount_of_iteration; i++) {
+				double result = 0;
+				for (int j = 0; j < amount; j++) {
+					result += F_result(Coefficients, Data[j]);
+				}
+				result = result / amount;
+				cout << "result is " << result << endl;
+				delta = coefficient_of_delta * (100 - result);
+				bool up = false;
+				for (int j = 0; j < amount; j++) {
+					int best_k = 0, best_x = 0;
+					double best_result = 0;
+					for (int k = 0; k < amount_of_coefficients; k++) {
+						for (int x = -1; x < 2; x += 2) {
+							Coefficients[(k - (k % 10)) / 10][k % 10] += delta * x;
+							double result_now = F_result(Coefficients, Data[j]);
+							Coefficients[(k - (k % 10)) / 10][k % 10] -= delta * x;
+							if (result_now > best_result) {
+								best_result = result_now;
+								best_k = k;
+								best_x = x;
+								up = true;
+							}
+						}
+					}
+					if (up) {
+						Coefficients[(best_k - (best_k % 10)) / 10][best_k % 10] += delta * best_x;
+					}
+					else {
+						cout << "up is failed" << endl;
+					}
+				}
+			}
+			double last_result = 0;
+			for (int j = 0; j < amount; j++) {
+				last_result += F_result(Coefficients, Data[j]);
+			}
+			last_result = last_result / amount;
+			cout << "last result is " << last_result << endl;
+			ofstream OFcoefficients("coefficient.txt");
+			for (int i = 0; i < ((amount_of_level - 1) * amount_of_neurons_per_level + 1); i++) {
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					OFcoefficients << Coefficients[i][j] << " ";
+				}
+				OFcoefficients << endl;
+			}
+			OFcoefficients.close();
+		}
+		void Get_result(int number) {
+			double** Coefficients = Read_coefficients();
+			double** Data = Read_Train();
+			cout << F_result(Coefficients, Data[number - 1]) << endl;
+		}
+		void Learn_2(int amount_of_iteration, double coefficient_of_delta) {
+			double** Coefficients = Read_coefficients();
+			double** Data = Read_Train();
+			double** Error_per_neuron = new double* [amount_of_level - 1];
+			double** New_coefficients = new double* [(amount_of_level - 1) * amount_of_neurons_per_level + 1];
+			for (int i = 0; i < amount_of_level - 1; i++) {
+				Error_per_neuron[i] = new double[amount_of_neurons_per_level];
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					Error_per_neuron[i][j] = 0;
+				}
+			}
+			for (int i = 0; i < (amount_of_level - 1) * amount_of_neurons_per_level + 1; ++i) {
+				New_coefficients[i] = new double[amount_of_neurons_per_level];
+			}
+			double delta = coefficient_of_delta, error, whole_coef;
+			int amount_of_coefficients = ((amount_of_level - 1) * amount_of_neurons_per_level + 1) * amount_of_neurons_per_level; //without last level
+			for (int z = 0; z < amount_of_iteration; ++z) {
+				for (int i = 0; i < amount; i++) {
+					//cout << F_result(Coefficients, Data[i]) << " ";
+					error = 100 - F_result(Coefficients, Data[i]);
+					whole_coef = 0;
+					for (int j = 0; j < amount_of_neurons_per_level; j++) {
+						whole_coef += abs(Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j]);
+					}
+					for (int j = 0; j < amount_of_neurons_per_level; j++) {
+						Error_per_neuron[amount_of_level - 2][j] = Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] * error / whole_coef;
+						Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] += Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] * error * delta / whole_coef;
+						if (F_result(Coefficients, Data[i]) + error > 100) {
+							New_coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] = Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] + Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] * error * delta / whole_coef;
+						}
+						else {
+							New_coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] = Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] - Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] * error * delta / whole_coef;
+						}
+						Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] -= Coefficients[(amount_of_level - 1) * amount_of_neurons_per_level][j] * error * delta / whole_coef;
+					}
+					for (int j = amount_of_level - 3; j > -1; j--) {
+						for (int k = 0; k < amount_of_neurons_per_level; k++) {
+							whole_coef = 0;
+							for (int x = 0; x < amount_of_neurons_per_level; x++) {
+								whole_coef += abs(Coefficients[(j + 1) * 10 + k][x]);
+							}
+							for (int x = 0; x < amount_of_neurons_per_level; x++) {
+								Error_per_neuron[j][k] += Coefficients[(j + 1) * 10 + k][x] * Error_per_neuron[j + 1][x] / whole_coef;
+								Coefficients[j * 10 + k][x] += Coefficients[j * 10 + k][x] * Error_per_neuron[j + 1][x] * delta / whole_coef;
+								if (F_result(Coefficients, Data[i]) + error > 100) {
+									New_coefficients[(j + 1) * 10 + k][x] = Coefficients[(j + 1) * 10 + k][x] + Coefficients[(j + 1) * 10 + k][x] * Error_per_neuron[j + 1][x] * delta / whole_coef;
+								}
+								else {
+									New_coefficients[(j + 1) * 10 + k][x] = Coefficients[(j + 1) * 10 + k][x] - Coefficients[(j + 1) * 10 + k][x] * Error_per_neuron[j + 1][x] * delta / whole_coef;
+								}
+								Coefficients[(j + 1) * 10 + k][x] -= Coefficients[(j + 1) * 10 + k][x] * Error_per_neuron[j + 1][x] * delta / whole_coef;
+							}
+						}
+					}
+					for (int k = 0; k < amount_of_neurons_per_level; k++) {
+						whole_coef = 0;
+						for (int x = 0; x < amount_of_neurons_per_level; x++) {
+							whole_coef += abs(Coefficients[k][x]);
+						}
+						for (int x = 0; x < amount_of_neurons_per_level; x++) {
+							Coefficients[k][x] += Coefficients[k][x] * Error_per_neuron[0][x] * delta / whole_coef;
+							if (F_result(Coefficients, Data[i]) + error > 100) {
+								New_coefficients[k][x] = Coefficients[k][x] + Coefficients[k][x] * Error_per_neuron[0][x] * delta / whole_coef;
+							}
+							else {
+								New_coefficients[k][x] = Coefficients[k][x] - Coefficients[k][x] * Error_per_neuron[0][x] * delta / whole_coef;
+							}
+							Coefficients[k][x] -= Coefficients[k][x] * Error_per_neuron[0][x] * delta / whole_coef;
+						}
+					}
+					cout << error << endl;
+					for (int ii = 0; ii < amount_of_level - 1; ++ii) {
+						for (int iii = 0; iii < amount_of_neurons_per_level; ++iii) {
+							cout << Error_per_neuron[ii][iii] << " ";
+						}
+						cout << endl;
+					}
+					for (int j = 0; j < (amount_of_level - 1) * amount_of_neurons_per_level + 1; ++j) {
+						for (int k = 0; k < amount_of_neurons_per_level; ++k) {
+							Coefficients[j][k] = New_coefficients[j][k];
+						}
+					}
+					//cout << F_result(Coefficients, Data[i]) << "\n";
+				}
+			}
+			ofstream OFcoefficients("coefficient.txt");
+			for (int i = 0; i < ((amount_of_level - 1) * amount_of_neurons_per_level + 1); i++) {
+				for (int j = 0; j < amount_of_neurons_per_level; j++) {
+					OFcoefficients << Coefficients[i][j] << " ";
+				}
+				OFcoefficients << endl;
+			}
+			OFcoefficients.close();
+		}
+};
+int main() {
+	NeuralNetwork A1(6, 10, 1);
+	A1.Create_Coefficients();
+	A1.Learn_2(1, 0.1);
+}
